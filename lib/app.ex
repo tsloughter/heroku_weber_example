6ac59959f8cb774cd5375fb25a6192407ec39b67
0xAX <testtest.89@mail.ru>
2013-11-07 00:12:11 +0600

    Update app.ex

--- lib/app.ex
+++ lib/app.ex
@@ -1,10 +1,7 @@
 defmodule TestWeber_2 do
 
-  import Weber
-
   def start(_type, _args) do
-    {:ok, root} = :file.get_cwd()
-    run_weber(:test_weber_2, Route.__route__, root, Config.config)
+    Weber.run_weber
   end
 
   def stop(_state) do
